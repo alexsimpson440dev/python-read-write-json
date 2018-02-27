958e4e90af18a82b33d913c05414b0c20d284ca3
Alexander Simpson <alexander.simpson@target.com>
2018-02-27 13:44:04 -0600

    Added writing to a file functions.

---
+++ __main__.py
@@ -0,0 +1,13 @@
+from src.read import Read
+from src.write import Write
+
+read = Read()
+write = Write()
+
+
+def main():
+    write.add_data()
+
+
+if __name__ == '__main__':
+    main()
--- src/write.py
+++ src/write.py
@@ -1,6 +1,22 @@
+import json
+
+
 class Write(object):
     def __init__(self):
         pass
-    
-    def bar(self):
-        pass
+
+    def add_data(self):
+        data = \
+            {
+                'name': 'John',
+                'age': 23,
+                'username': 'johndoe440',
+                'password': 'testing'
+            }
+
+        self.dump_data(data)
+
+    def dump_data(self, data):
+        print(data)
+        with open('test.json', 'w') as file:
+            json.dump(data, file)
